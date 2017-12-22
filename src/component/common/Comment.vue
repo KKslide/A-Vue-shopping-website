737aa62877184c87bed9c38bd922be332b9bff32
kk <848744804@qq.com>
2017-12-22 20:38:45 +0800

    搞定商品评论功能

--- src/component/common/Comment.vue
+++ src/component/common/Comment.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="comment-box">
         <!--取得评论总数-->
-        <form id="commentForm" name="commentForm" class="form-box" @click.prevent="subComment">
+        <form id="commentForm" name="commentForm" class="form-box" @submit.prevent="subComment">
             <div class="avatar-box">
                 <i class="iconfont icon-user-full"></i>
             </div>
             <div class="conn-box">
                 <div class="editor">
-                    <textarea id="txtContent" name="txtContent" sucmsg=" " datatype="*10-1000" nullmsg="请填写评论内容！"></textarea>
+                    <textarea v-model="commentContent" id="txtContent" name="txtContent" sucmsg=" " 
+                    datatype="*10-1000" nullmsg="请填写评论内容！"></textarea>
                     <span class="Validform_checktip"></span>
                 </div>
                 <div class="subcon">
-                    <input id="btnSubmit" name="submit" type="submit" value="提交评论" class="submit">
+                    <!-- <input id="btnSubmit" name="submit" type="submit" value="提交评论" class="submit"> -->
+                    <button id="btnSubmit" name="submit" type="submit" class="submit">提交评论</button>
                     <span class="Validform_checktip"></span>
                 </div>
             </div>
         </form>
         <ul id="commentList" class="list-box">
-            <p style="margin:5px 0 15px 69px;line-height:42px;text-align:center;border:1px solid #f7f7f7;">暂无评论，快来抢沙发吧！</p>
+            <p v-if="!commentList.length" style="margin:5px 0 15px 69px;line-height:42px;text-align:center;border:1px solid #f7f7f7;">暂无评论，快来抢沙发吧！</p>
             <li v-for="item in commentList" :key="item.id">
                 <div class="avatar-box">
                     <i class="iconfont icon-user-full"></i>
@@ -33,11 +35,9 @@
         </ul>
         <!--放置页码-->
         <div class="page-box" style="margin:5px 0 0 62px">
-            <div id="pagination" class="digg">
-                <span class="disabled">« 上一页</span>
-                <span class="current">1</span>
-                <span class="disabled">下一页 »</span>
-            </div>
+
+            <el-pagination layout="prev, pager, next" :total="10*this.totalcount" @current-change="handleCurrentChange"> </el-pagination>
+
         </div>
         <!--/放置页码-->
     </div>
@@ -48,24 +48,56 @@ export default {
     props: ['tablename', 'artID'],
     data() {
         return {
+            /* 评论框内容 */
+            commentContent: '',
             /* 评论列表 */
             commentList: [],
             query: {
                 pageIndex: 1,
-                pageSize: 10
-            }
+                pageSize: 5
+            },
+            totalcount: 0
         }
     },
     methods: {
         getCommentList() {
             this.$http.get(
-                this.$api.commentList + this.tablename + "/" + this.artId,
+                this.$api.commentList + this.tablename + '/' + this.artID,
                 { params: this.query }
             ).then(res => {
                 this.commentList = res.data.message;
+                this.totalcount = res.data.totalcount;
+                console.log(this.totalcount);
             });
         },
-        subComment() { },
+        /* 更改当前页 */
+        handleCurrentChange() {
+            // this.pageIndex
+        },
+        subComment() {
+            this.$http.post(
+                this.$api.comment + this.tablename + '/' + this.artID,
+                { commenttxt: this.commentContent }
+            ).then(res => {
+                // 评论成功的提示
+                this.$message({
+                    message: '恭喜你，发表成功',
+                    type: 'success'
+                });
+                /* 假刷新一下评论 */
+                this.commentList.unshift({
+                    user_name: '前端大神',
+                    user_ip: '127.0.0.1',
+                    add_time: new Date(),
+                    content: this.commentContent
+                });
+                this.commentContent = '';
+            });
+        },
+
+    },
+    created() {
+        this.getCommentList();
     }
 }
 </script>
--- src/component/goods/detail/frame/LeftImg.vue
+++ src/component/goods/detail/frame/LeftImg.vue
@@ -36,7 +36,7 @@ import '../../../../lib/imgzoom/magnifier.js';
 import $ from 'jquery';
 
 export default {
-    porps: ['imglist'],
+    props: ['imglist'],
     mounted() {
         // setTimeout(function() {
         //     var _magnifier = $().imgzoon({
@@ -53,10 +53,10 @@ export default {
             this.$nextTick(function() {
                 var _magnifier = $().imgzoon({
                     magnifier: "#magnifier1",//最外层的大容器
-                    width: 370,//承载容器宽
-                    height: 370,//承载容器高
+                    width: 350,//承载容器宽
+                    height: 350,//承载容器高
                     moveWidth: null,//如果设置了移动盒子的宽度，则不计算缩放比例
-                    zoom: 5//缩放比例
+                    zoom: 2//缩放比例
                 });
             });
         }

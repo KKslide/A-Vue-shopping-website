adf1f8c062e7bfdda007a944aa37b82bd2271b21
KKslide <rollercompanion@163.com>
2019-03-15 10:29:29 +0800

    完成css引入和header组件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
-  <h1>test</h1>
+  <app-header></app-header>
 </template>
 
 <script>
+import HeaderComponent from './components/frame/Header'
+import $ from 'jquery'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    appHeader: HeaderComponent
+  },
+  mounted() {
+    $("#menu2 li a").wrapInner('<span class="out"></span>');
+    $("#menu2 li a").each(function () {
+      $('<span class="over">' + $(this).text() + '</span>').appendTo(this);
+    });
+    $("#menu2 li a").hover(function () {
+      $(".out", this).stop().animate({ 'top': '48px' }, 300); // move down - hide
+      $(".over", this).stop().animate({ 'top': '0px' }, 300); // move down - show
+    }, function () {
+      $(".out", this).stop().animate({ 'top': '0px' }, 300); // move up - show
+      $(".over", this).stop().animate({ 'top': '-48px' }, 300); // move up - hide
+    });
+  }
 }
 </script>
 
---
+++ src/components/frame/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="header" class="header">
+    <div class="head-top">
+      <div class="section">
+        <div class="left-box">
+          <span>vue2.0单页应用</span>
+          <a target="_blank" href="#"></a>
+          <a target="_blank" href="#"></a>
+        </div>
+        <div id="menu" class="right-box">
+          <a href="/login.html">登录</a>
+          <a href="/register.html">注册</a>
+          <strong>|</strong>
+          <!--<a href="/content/contact.html"><i class="iconfont icon-phone"></i>联系我们</a>
+          <a href="/cart.html"><i class="iconfont icon-cart"></i>购物车(<span id="shoppingCartCount"><script type="text/javascript" src="/tools/submit_ajax.ashx?action=view_cart_count"></script></span>)</a>-->
+        </div>
+      </div>
+    </div>
+    <div class="head-nav">
+      <div class="section">
+        <!-- <div class="logo">
+                       <a href="/index.html"><img width="230px" height="70px" src="/templates/main/images/logo.png" /></a>
+        </div>-->
+        <div id="menu2" class="nav-box menuhd">
+          <ul>
+            <li class="index">
+              <a href="/index.html">HTML5</a>
+            </li>
+            <li class="news">
+              <a href="/news.html">CSS3</a>
+            </li>
+            <li class="photo">
+              <a href="/photo.html">JavaScript</a>
+            </li>
+            <li class="video">
+              <a href="/video.html">Angular</a>
+            </li>
+            <li class="down">
+              <a href="/down.html">Vue</a>
+            </li>
+            <li>
+              <a target="_blank" href="/admin/index.aspx">ReactNative</a>
+            </li>
+          </ul>
+        </div>
+        <div class="search-box">
+          <div class="input-box">
+            <input
+              id="keywords"
+              name="keywords"
+              type="text"
+              onkeydown="if(event.keyCode==13){SiteSearch('/search.html', '#keywords');return false};"
+              placeholder="输入关健字"
+              x-webkit-speech
+            >
+          </div>
+          <a href="javascript:;" onclick="SiteSearch('/search.html', '#keywords');">
+            <i class="iconfont icon-search"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>

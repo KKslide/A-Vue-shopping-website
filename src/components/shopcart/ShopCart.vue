d02fbe383556dd08611409fa358d1d9e3a0d175f
KKslide <rollercompanion@163.com>
2019-03-19 16:27:12 +0800

    完成购物车全选和属性计算

--- src/components/shopcart/ShopCart.vue
+++ src/components/shopcart/ShopCart.vue
@@ -53,7 +53,12 @@
               <tbody>
                 <tr>
                   <th width="48" align="center">
-                    <a>全选</a>
+                    <!-- 设置value属性 -->
+                    <el-switch
+                      :value="allSwitchState"
+                      @change="switchAllChange"
+                      active-color="#13ce66"
+                    ></el-switch>
                   </th>
                   <th align="left" colspan="2">商品信息</th>
                   <th width="84" align="left">单价</th>
@@ -61,7 +66,32 @@
                   <th width="104" align="left">金额(元)</th>
                   <th width="54" align="center">操作</th>
                 </tr>
-                <tr>
+                <!-- 商品列表 -->
+                <tr v-for="item in goodsList" :key="item.id">
+                  <td width="48" align="center">
+                    <el-switch v-model="item.selected" active-color="#13ce66"></el-switch>
+                  </td>
+                  <td align="left" colspan="2">
+                    <img width="50" height="50" :src="item.img_url" alt>
+                    <span>{{ item.title }}</span>
+                  </td>
+                  <td width="84" align="left">￥{{ item.sell_price }}</td>
+                  <td width="104" align="center">
+                    <el-input-number
+                      v-model="item.buycount"
+                      @change="numberChange(item.id, $event)"
+                      size="mini"
+                      :min="1"
+                    ></el-input-number>
+                  </td>
+                  <td width="104" align="left">￥{{ item.sell_price * item.buycount }}</td>
+                  <td width="54" align="center">
+                    <el-button size="mini" @click.native="delGoods(item.id)">删除</el-button>
+                  </td>
+                </tr>
+                <!-- 商品列表 -->
+                <!-- 没有添加商品时的提示 -->
+                <tr v-if="!goodsList.length">
                   <td colspan="10">
                     <div class="msg-tips">
                       <div class="icon warning">
@@ -77,14 +107,17 @@
                     </div>
                   </td>
                 </tr>
+                <!-- 没有添加商品时的提示 -->
+                <!-- 总价和运费 -->
                 <tr>
                   <th align="right" colspan="8">
                     已选择商品
-                    <b class="red" id="totalQuantity">0</b> 件 &nbsp;&nbsp;&nbsp; 商品总金额（不含运费）：
+                    <b class="red" id="totalQuantity">{{selectedShopcartTotal}}</b> 件 &nbsp;&nbsp;&nbsp; 商品总金额（不含运费）：
                     <span class="red">￥</span>
-                    <b class="red" id="totalAmount">0</b>元
+                    <b class="red" id="totalAmount">{{selectedShopcartTotalPrice}}</b>元
                   </th>
                 </tr>
+                <!-- 总价和运费 -->
               </tbody>
             </table>
           </div>
@@ -105,21 +138,51 @@
 
 <script>
 export default {
-    data(){
-        return{
-            goodsList:[]
-        }
+  data() {
+    return {
+      goodsList: []
+    }
+  },
+  computed: {
+    //   已选商品数量
+    selectedShopcartTotal() {
+      return this.goodsList.reduce((p, c) => c.selected ? p + c.buycount : p, 0)
     },
-    methods:{
-        getGoodsList(){
-            this.$http.get(this.$api.shopcarGoods+this.$store.getters.getShopcartIDs)
-            .then(res=>{
-                console.log(res);
-            })
-        }
+    // 总价
+    selectedShopcartTotalPrice() {
+      return this.goodsList.reduce((p, c) => c.selected ? p + c.sell_price * c.buycount : p, 0)
     },
+    // 全选状态
+    allSwitchState() {
+      return this.goodsList.every(v => v.selected)
+    }
+  },
+  methods: {
+    getGoodsList() {
+      this.$http.get(this.$api.shopcarGoods + this.$store.getters.getShopcartIDs)
+        .then(res => {
+          res.data.message.forEach(goods => {
+            goods.selected = true
+            goods.buycount = this.$store.getters.getShopcartData[goods.id]
+          })
+          this.goodsList = res.data.message
+        })
+    },
+    // 数量更改
+    numberChange(id, val) {
+      this.$store.commit('upShopcartData', { id: id, val: val })
+    },
+    // 删除商品
+    delGoods(id) {
+      this.$store.commit('delShopcartData', { id: id })
+      this.goodsList = this.goodsList.filter(v => v.id != id)
+    },
+    // 全选按钮
+    switchAllChange(bol) {
+      this.goodsList.forEach(v => v.selected = bol)
+    }
+  },
   created() {
-    // console.log(this.$store.getters.getShopcartIDs);
     this.getGoodsList();
   }
 }
